684165cc1e36f7edf2e503fb39610b162fb5d4e4
GalihN7 <124852405+GalihN7@users.noreply.github.com>
2024-10-20 12:54:12 +0700

    Database Wrapper
    Video Membuat Aplikasi MVC dengan PHP #8 Database Wrapper

--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -3,14 +3,14 @@
     <div class="row">
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
                 <?php foreach($data['$mhs'] as $mhs ) : ?>
-                    <ul>
-                        <li> <? $mhs['nama'];?></li>
-                        <li> <? $mhs['nrp'];?>/li>
-                        <li> <? $mhs['email'];?></li>
-                        <li> <? $mhs['jurusan'];?></li>
-                    </ul>
+                    <li class="list-group-item">
+                        <?= $mhs['nama']; ?>
+                        <a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs['id'];?>" class="badge text-bg-primary d-flex justify-content-between align-items-start">detail</a>
+                    </li>
                 <?php endforeach; ?>
+            </ul>
         </div>
     </div>
 
