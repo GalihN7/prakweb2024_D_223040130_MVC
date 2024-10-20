f74f9f0cc3cdc34cd57c440567be4360f9385553
GalihN7 <124852405+GalihN7@users.noreply.github.com>
2024-10-20 14:01:34 +0700

    Delete Data
    Video Membuat Aplikasi MVC dengan PHP #11 Delete Data

--- phpmvc/app/controllers/Mahasiswa.php
+++ phpmvc/app/controllers/Mahasiswa.php
@@ -32,5 +32,19 @@ class Mahasiswa extends Controller{
             exit;
         }
     }
+
+    public function hapus($id)
+    {
+        if($this->model('Mahasiswa_model')->hapushDataMahasiswa($id) > 0)
+         {
+            Flasher::setFlash('berhasil','dihapus', 'success');
+            header('location:' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal','dihapus', 'danger');
+            header('location:' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
     
 }
