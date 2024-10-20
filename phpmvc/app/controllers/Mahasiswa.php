c6e4d61addb1527d5c21ded251c44a833941b457
GalihN7 <124852405+GalihN7@users.noreply.github.com>
2024-10-20 13:49:10 +0700

    Flash Message
    Video Membuat Aplikasi MVC dengan PHP #10 Flash Message

--- phpmvc/app/controllers/Mahasiswa.php
+++ phpmvc/app/controllers/Mahasiswa.php
@@ -21,8 +21,15 @@ class Mahasiswa extends Controller{
 
     public function tambah()
     {
-        if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
+        if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0)
+         {
+            Flasher::setFlash('berhasil','ditambahkan', 'success');
             header('location:' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal','ditambahkan', 'danger');
+            header('location:' . BASEURL . '/mahasiswa');
+            exit;
         }
     }
     
